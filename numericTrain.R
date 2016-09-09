984cd1068055b8365759d03b1ae91da8f7c584f2
romagm <romangmuer@gmail.com>
2016-09-09 12:36:42 +0200

    numericTrain verbessert

--- numericTrain.R
+++ numericTrain.R
@@ -1,29 +1,7 @@
 # fread (data.table package) to load csv faster
 
 library(data.table)
-train_numeric <- fread("data/train_numeric.csv",header=TRUE,stringsAsFactors=TRUE,data.table=FALSE)
+train_numeric <- fread("data/train_numeric.csv",header=TRUE)
 
-
-# resampling to get at least 50 percent values 1
-
-library(unbalanced)
-
-splittingIO <- function(d,nvar,y){
-  nvar <- nvar - 1
-  dataNames <- names(d)
-  inputNames <- dataNames[1:nvar]
-  input <<- d[,inputNames]
-  output <<- d[,as.character(y)]
-}
-
-splittingIO(train_numeric,970,"Response")
-
-results <- ubUnder(X=input,Y=output,perc=50,method="percPos")
-rm(train_numeric,input,output)
-gc()
-train.numeric <- results$X
-train.numeric$Response <- results$Y
-rm(results)
-gc()
-summary(train.numeric)
-id_list <- train.numeric$Id
+# cleaning and exploring data with dtplyr
+library(dplyr)
